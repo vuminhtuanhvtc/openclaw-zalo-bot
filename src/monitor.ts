db56d3203edfe24c41e9faaed6b2d27c90c3ea79
vuminhtuanhvtc <vuminhtuanhvtc@users.noreply.github.com>
2026-02-28 15:25:34 +0700

    revert: remove incorrect accountId arg from readAllowFromStore to match original zalo pattern

--- src/monitor.ts
+++ src/monitor.ts
@@ -357,7 +357,7 @@ async function processMessageWithPipeline(params: {
     configuredAllowFrom: configAllowFrom,
     senderId,
     isSenderAllowed: isZaloSenderAllowed,
-    readAllowFromStore: () => core.channel.pairing.readAllowFromStore("zalo_bot", account.accountId),
+    readAllowFromStore: () => core.channel.pairing.readAllowFromStore("zalo_bot"),
     shouldComputeCommandAuthorized: (body, cfg) =>
       core.channel.commands.shouldComputeCommandAuthorized(body, cfg),
     resolveCommandAuthorizedFromAuthorizers: (params) =>
